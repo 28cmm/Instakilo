e4ff77c22d6d9848a3cd280f03a55563ad9af354
28cmm <joshuafang1998@gmail.com>
2019-01-23 11:24:40 -0800

    Initial Commit

---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Yilei Huang on 2019-01-23.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
